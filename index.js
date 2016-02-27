5cada0c4ce72b20428b73b7eefa02922142f1fce
Ryan Clark <ryan.clark.j@gmail.com>
2016-02-27 16:56:23 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * This is a function that needs extensive documentation
+ *
+ * @param {number} num - a number
+ * @returns {boolean} whether or not the number is even
+ */
+function isEven(num) {
+  return !(num % 2);
+}
