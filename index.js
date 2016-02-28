f29bb5e7994c5032cd974172e34db09fd907dbc5
Ryan Clark <ryan.clark.j@gmail.com>
2016-02-27 17:17:27 -0700

    odd function

--- index.js
+++ index.js
@@ -7,3 +7,13 @@
 function isEven(num) {
   return !(num % 2);
 }
+
+/**
+ * This is a function that also needs extensive documentation
+ *
+ * @param {number} num - a number
+ * @returns {boolean} whether or not the number is odd
+ */
+function isOdd(num) {
+  return !!(num % 2);
+}
